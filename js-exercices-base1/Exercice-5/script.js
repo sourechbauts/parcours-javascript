a7d9c69a453d923a31c16b4284c2e5142174f340
Sourech Bauts <s.bauts@gmail.com>
2018-06-06 16:47:47 +0200

    ex5

---
+++ js-exercices-base1/Exercice-5/script.js
@@ -0,0 +1,5 @@
+var nombre1 = prompt('Premier nombre a virgule ?');
+var nombre2 = prompt('Deuxieme nombre a virgule ?');
+
+var resultat = parseInt(nombre1) + parseInt(nombre2);
+alert("La somme de ces deux nombres est " + resultat);
