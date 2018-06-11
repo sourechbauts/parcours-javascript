905f12421a6e18c0c278db95ab1e489563bd90a3
Sourech Bauts <s.bauts@gmail.com>
2018-06-11 11:31:24 +0200

    ex1change

--- js-exercices-base2/Exercice-1/script.js
+++ js-exercices-base2/Exercice-1/script.js
@@ -1,17 +1,15 @@
-function surfaceRectangle()
-{
-  var largeur = prompt("Saisissez une largeur:");
-  var longueur = prompt("Saisissez une longueur:");
-  var surface = largeur * longueur;
+function surfaceRectangle() {
+    var largeur = prompt("Saisissez une largeur:");
+    var longueur = prompt("Saisissez une longueur:");
+    var surface = largeur * longueur;
 
-  alert("la surface du rectangle est de: "+surface);
+    alert("la surface du rectangle est de: " + surface);
 }
 
-function periRectangle()
-{
-  var largeur = prompt("Saisissez une largeur:");
-  var longueur = prompt("Saisissez une longueur:");
-  var perimetre = (largeur * 2) + (longueur * 2);
+function periRectangle() {
+    var largeur = prompt("Saisissez une largeur:");
+    var longueur = prompt("Saisissez une longueur:");
+    var perimetre = (largeur * 2) + (longueur * 2);
 
-  alert("le périmetre du rectangle est de: "+perimetre);
+    alert("le périmetre du rectangle est de: " + perimetre);
 }
