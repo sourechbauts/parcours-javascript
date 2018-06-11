852d96f2da6df81c69a3aa7bc9c0a50052899f0b
Sourech Bauts <s.bauts@gmail.com>
2018-06-11 10:12:11 +0200

    ex7

---
+++ js-exercices-base1/Exercice-7/script.js
@@ -0,0 +1,13 @@
+var pointure = prompt("Quel est ta pointure ?");
+var dateAn = prompt("Quel est ton année de naissance ?");
+
+function total(){
+  var resultat = (pointure *=2) +5;
+  resultat*= 50;
+  resultat-= dateAn;
+  resultat+= 1766;
+
+  return  resultat;
+};
+
+alert(total());
